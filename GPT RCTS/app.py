04b77ecf3054bd557d0dbb7d0c0dce2236290093
Praveen Kumar Palaboyina <113330666+praveenkumar911@users.noreply.github.com>
2024-02-15 11:43:16 +0530

    Update app.py

--- GPT RCTS/app.py
+++ GPT RCTS/app.py
@@ -1,25 +1,24 @@
-from flask import Flask, render_template, request
-
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from flask import Flask, request, jsonify
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 app = Flask(__name__)
 
 model_name_or_path = "TheBloke/Llama-2-7b-Chat-GPTQ"
-model = AutoModelForCausalLM.from_pretrained(model_name_or_path,
-                                             device_map="auto",
-                                             trust_remote_code=False,
-                                             revision="main")
+model = AutoModelForCausalLM.from_pretrained(
+    model_name_or_path,
+    device_map="auto",
+    trust_remote_code=False,
+    revision="main"
+)
 
 tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        prompt = request.form['prompt']
-        response = generate_response(prompt)
-        return render_template('index.html', prompt=prompt, response=response)
-    else:
-        return render_template('index.html', prompt='', response='')
+@app.route('/generate', methods=['POST'])
+def generate():
+    data = request.get_json()
+    prompt = data.get('prompt', '')
+    response = generate_response(prompt)
+    return jsonify({'response': response})
 
 def generate_response(prompt):
     prompt_template = f'''[INST] <<SYS>>
@@ -32,5 +31,4 @@ Answer Whatever Asked
     return tokenizer.decode(output[0])
 
 if __name__ == '__main__':
-    # Run the app on port 6030
     app.run(debug=True, port=6030)
